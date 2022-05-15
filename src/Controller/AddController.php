df17854f361ec9f962710ea88d734cdad4f604b6
mahdichaari01 <92325467+mahdichaari01@users.noreply.github.com>
2022-05-15 18:29:47 +0100

    etape 1 fonctionne

--- src/Controller/AddController.php
+++ src/Controller/AddController.php
@@ -2,17 +2,44 @@
 
 namespace App\Controller;
 
+use App\Entity\Etudiant;
+use App\Entity\Section;
+use App\Form\AddEtudiantType;
+use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AddController extends AbstractController
 {
     #[Route('/add', name: 'app_add')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine,Request $request): Response
     {
-        return $this->render('add/index.html.twig', [
-            'controller_name' => 'AddController',
+        $manager=$doctrine->getManager();
+
+        $new = new Etudiant();
+        $new->setNom("nom")->setPrenom("prenom")->setSection(null);
+
+        $form = $this->createForm(AddEtudiantType::class, $new);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $new = $form->getData();
+            $manager->persist($new);
+            $manager->flush();
+            // ... perform some action, such as saving the task to the database
+            $this->addFlash("info", "insertion with success");
+            return $this->redirectToRoute('app_list_all');
+        }
+
+
+        return $this->renderForm('add/index.html.twig', [
+            'form' => $form,
         ]);
+
     }
 }
---
+++ src/Form/AddEtudiantType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Etudiant;
+use App\Entity\Section;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AddEtudiantType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('prenom')
+            ->add(Section::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Etudiant::class,
+        ]);
+    }
+}
